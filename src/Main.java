3b76acc2a4e54f0e892562872efd863dabe168cc
Trevor Marsh <gatheringshadow@gmail.com>
2021-02-05 23:21:17 -0700

    Got the for loop outputting all values
    Fixed .gitignore to leave out build files from IDEA

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
 		Map<String, String[]> people = new HashMap<>();
 		people.put("Daiman", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
-				"understands code", "Doesn/'t understand code" });
+				"understands code", "Doesn't understand code" });
 		people.put("Trevor", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
-				"understands code", "Doesn/'t understand code" });
+				"understands code", "Doesn't understand code" });
 		people.put("Riley", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
-				"understands code", "Doesn/'t understand code" });
+				"understands code", "Doesn't understand code" });
 		people.put("Autumn", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
-				"understands code", "Doesn/'t understand code" });
+				"understands code", "Doesn't understand code" });
 		people.put("Jesse", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
-				"understands code", "Doesn/'t understand code" });
-		System.out.println(people);
-		//for (Entry<String, String[]> person : people) {
-			//System.out.println(person.key() + " says " + person.value());
-		//}
+				"understands code", "Doesn't understand code"});
+
+		for (Map.Entry<String, String[]> person : people.entrySet()) {
+			System.out.println(person.getKey() + " says: " + Arrays.toString(person.getValue()));
+		}
 	}
 }
