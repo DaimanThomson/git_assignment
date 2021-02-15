5c1df00ef1d60c4b59a8a49a0013806b8073f624
Trevor Marsh <gatheringshadow@gmail.com>
2021-02-15 16:11:38 -0700

    Changed to only one conversation

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,6 @@ public class Main {
 		Map<String, String[]> people = new HashMap<>();
 		people.put("Daiman", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
 				"understands code", "Doesn't understand code" });
-		people.put("Trevor", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
-				"understands code", "Doesn't understand code" });
-		people.put("Riley", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
-				"understands code", "Doesn't understand code" });
 		people.put("Autumn", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
 				"understands code", "Doesn't understand code" });
 		people.put("Jesse", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
@@ -20,8 +16,6 @@ public class Main {
 
 		String person1 = null;
 		String person2 = null;
-		String person3 = null;
-		String person4 = null;
 		for (Map.Entry<String, String[]> person : people.entrySet()) {
 			if (null == person1) {
 				person1 = person.getKey();
@@ -29,17 +23,9 @@ public class Main {
 			else if (null == person2) {
 				person2 = person.getKey();
 			}
-			else if (null == person3) {
-				person3 = person.getKey();
-			}
-			else if (null == person4) {
-				person4 = person.getKey();
-			}
 		}
 
 		converse(person1, person2, people);
-		System.out.println();
-		converse(person3, person4, people);
 	}
 
 	private static void converse(String name1, String name2, Map<String, String[]> people) {
