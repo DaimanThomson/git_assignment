f83e297c93b6ef1e14214e567adcb3d03d2f1253
Trevor Marsh <gatheringshadow@gmail.com>
2021-02-15 15:08:58 -0700

    Converse between two pairs

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,37 @@ public class Main {
 		people.put("Jesse", new String[] { "wins the lottery", "loses the lottery", "drives to Idaho",
 				"understands code", "Doesn't understand code"});
 
+		String person1 = null;
+		String person2 = null;
+		String person3 = null;
+		String person4 = null;
 		for (Map.Entry<String, String[]> person : people.entrySet()) {
-			System.out.println(person.getKey() + " says: " + Arrays.toString(person.getValue()));
+			if (null == person1) {
+				person1 = person.getKey();
+			}
+			else if (null == person2) {
+				person2 = person.getKey();
+			}
+			else if (null == person3) {
+				person3 = person.getKey();
+			}
+			else if (null == person4) {
+				person4 = person.getKey();
+			}
+		}
+
+		converse(person1, person2, people);
+		System.out.println();
+		converse(person3, person4, people);
+	}
+
+	private static void converse(String name1, String name2, Map<String, String[]> people) {
+		String[] phrases1 = people.get(name1);
+		String[] phrases2 = people.get(name2);
+
+		for (int i = 0; i < phrases1.length; i++) {
+			System.out.println(String.join("", name1, ": ", phrases1[i]));
+			System.out.println(String.join("", name2, ": ", phrases2[i]));
 		}
 	}
 }
